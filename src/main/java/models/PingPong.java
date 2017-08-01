537c7d0d91eb25ad14c68daede291aa6b55322b9
Trevor Gill <trevor.a.gill@gmail.com>
2017-07-31 22:04:15 -0700

    refactor ping pong project with test classes

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -1,4 +1,21 @@
 package models;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PingPong {
+    public ArrayList<Object> runPingPong(int countUpTo){
+        ArrayList<Object> result = new ArrayList<Object>();
+        for (int i = 1; i <= countUpTo; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("pingpong");
+            } else if (i % 3 == 0){
+                result.add("ping");
+            } else if (i % 5 == 0){
+                result.add("pong");
+            } else {
+                result.add(i);
+            }
+        }
+        return result;
+    }
 }
